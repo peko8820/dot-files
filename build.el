5e8d12d28f6c51afb71a4424ee40543571ddb931
Markus Sievers <markussievers@gmail.com>
2016-05-20 02:11:23 +0200

    First round of modifications work

--- build.el
+++ build.el
@@ -11,7 +11,7 @@
 ;; use the git bash terminal (if you need to suffer through windows)
 ;; or regular terminal use something like this:
 ;;
-;;     Emacs --load "./build.el"
+;;     emacs --load "./build.el"
 
 
 ;;; Code:
@@ -57,9 +57,55 @@
    ;; Some Elisp files are just symlinked instead of tangled...
   (ms/mksymlinks "${dot-files-src}/elisp/*.el"
                  "${ms/emacs-directory}/elisp")
-  )
+
+  ;; Just link the entire directory instead of copying the snippets:
+  (ms/mksymlink  "${dot-files-src}/snippets"
+                 "${ms/emacs-directory}/snippets")
+
+  ;; Just link the entire directory instead of copying the snippets:
+  (ms/mksymlink  "${dot-files-src}/templates/"
+                 "${ms/emacs-directory}/templates")
+
+  ;; Some Elisp files are just symlinked instead of tangled...
+  (ms/mksymlinks "${dot-files-src}/bin/[a-z]*"
+                 "${HOME}/bin")
+
+  ;; Yeah, this makes me snicker every time I see it. I have not yet created the folder inside the source directory, though
+  ;; (ms/mksymlink  "${dot-files-src}/vimrc" "${HOME}/.vimrc")
+
+  ;; All of the .el files I've eithe tangled or linked should be comp'd:
+  ;; (mapc 'byte-compile-file
+  ;;       (ms/get-files "${ms/emacs-directory}/elisp/*.el" t))
+
+  (message "Finished building dot-files- Resetting Emacs.")
+  (require 'init-main (ms/get-path "${user-emacs-directory}elisp/init-main.el")))
+
+(defun ms/tangle-file (file)
+  "Given an 'org-mode' FILE, tangle the source code."
+  (interactive "fOrg File: ")
+  (find-file file)   ;;  (expand-file-name file \"$DIR\")
+  (org-babel-tangle)
+  (kill-buffer))
 
 
+(defun ms/tangle-files (path)
+  "Given a directory, PATH, of 'org-mode' files, tangle source code out of all literate programming files."
+  (interactive "D")
+  (mapc 'ms/tangle-file (ms/get-files path)))
+
+
+(defun ms/get-dot-files ()
+  "Pull and build latest from the Github repository.  Load the resulting Lisp code."
+  (interactive)
+  (let ((git-results
+         (shell-command (concat "cd " dot-files-src "; git pull"))))
+    (if (not (= git-results 0))
+        (message "Can't pull the goodness. Pull from git by hand.")
+      (load-file (concat dot-files-src "/emacs.d/shell-script-funcs.el"))
+      (load-file (concat dot-files-src "/build.el"))
+      (require 'init-main))))
+
+(ms/build-dot-files)  ;; Do it
 
 (provide 'dot-files)
 ;;; build.el ends here
